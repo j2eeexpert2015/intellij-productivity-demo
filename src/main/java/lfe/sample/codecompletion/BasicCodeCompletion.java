f552a94a832cf2fd72068138a186d8485f768035
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2024-09-12 13:06:48 +0530

    Update BasicCodeCompletion.java

--- src/main/java/lfe/sample/codecompletion/BasicCodeCompletion.java
+++ src/main/java/lfe/sample/codecompletion/BasicCodeCompletion.java
@@ -1,5 +1,7 @@
 package lfe.sample.codecompletion;
 
+import lfe.sample.model.Student;
+
 /**
  * Start typing a name. By default, IntelliJ IDEA displays the code completion popup
  * automatically as you type.
@@ -7,5 +9,11 @@ package lfe.sample.codecompletion;
  * or choose Code -> Code Completion -> Basic from the main menu.
  */
 public class BasicCodeCompletion {
+    public String sampleText = "Sample Dummy Text ";
+    public void printSampleText() {
+        sampleText = sampleText.toLowerCase();
+        Student student = new Student("Peter",30,"Dummy Address");
+        //student.
+    }
 
 }
