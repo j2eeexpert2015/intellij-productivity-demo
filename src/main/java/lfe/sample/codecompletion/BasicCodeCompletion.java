aa6eaff0436c434b4298e7b5fe68e0b26e814b42
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2024-09-12 11:30:18 +0530

    New class added

---
+++ src/main/java/lfe/sample/codecompletion/BasicCodeCompletion.java
@@ -0,0 +1,11 @@
+package lfe.sample.codecompletion;
+
+/**
+ * Start typing a name. By default, IntelliJ IDEA displays the code completion popup
+ * automatically as you type.
+ * If automatic completion is disabled, press Ctrl + Space
+ * or choose Code -> Code Completion -> Basic from the main menu.
+ */
+public class BasicCodeCompletion {
+
+}
