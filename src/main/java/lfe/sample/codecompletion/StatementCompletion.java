1fd35aeb3cd7e21eed85bc120ac73a34767041cb
j2eeexpert2015 <j2eeexpert2015@gmail.com>
2024-09-12 21:00:52 +0530

    Added Code Completion Code

---
+++ src/main/java/lfe/sample/codecompletion/StatementCompletion.java
@@ -0,0 +1,39 @@
+package lfe.sample.codecompletion;
+
+import java.util.List;
+
+/**
+ * To complete a statement in IntelliJ IDEA, use the keyboard shortcut Ctrl+Shift+Enter
+ * <p>
+ * Complete statement can add your semicolons, brackets and manage caret placement for you.
+ * It also moves the caret to the next statement position.
+ */
+public class StatementCompletion {
+    private boolean flag = true;
+    private int i = 10;
+
+    List<String> items = null;
+    /**
+     * Complete a method declaration
+     *
+     * Start typing a method declaration and press Ctrl+Shift+Enter after the opening parenthesis.
+     */
+   
+    /**
+     * Complete a code construct
+     *
+     * Start typing a code construct and press Ctrl+Shift+Enter.
+     * IntelliJ IDEA automatically completes the construct and adds the required punctuation.
+     * The caret is placed at the next editing position.
+     */
+
+
+
+
+    /**
+     *  Wrap a method call argument
+     *
+     *  Type an expression. Then type a method call.
+     *  When println gets the focus in the suggestion list, select it with Ctrl+Shift+Enter:
+     */
+}
